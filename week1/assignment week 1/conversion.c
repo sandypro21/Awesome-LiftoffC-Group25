9ceaf83d3bbbfac6a8f23e3540479cb43e9ee2eb
mihir6025 <76690519+mihir6025@users.noreply.github.com>
2021-02-07 11:48:43 +0530

    submitting week 1 assignment

---
+++ week1/assignment week 1/conversion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int Ctemp,Ftemp;
+    printf("enter temp in celsius:");
+    scanf("%d", &Ctemp);
+    Ftemp=(Ctemp*9/5)+32;
+    printf("temp in fahrenheit is:%d",Ftemp);
+
+    return 0;
+}
