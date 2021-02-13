73c1500f15bcac59e71c8edc56d4f804fec88ee2
mihir6025 <76690519+mihir6025@users.noreply.github.com>
2021-02-13 22:42:32 +0530

    Add files via upload

---
+++ ques5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int i,j;
+    for(i=0;i<5;i++){
+        for(j=0;j<5;j++){
+            if(i==j||(i+j)==4){
+            printf("*");            
+            }
+            else{
+                printf(" ");
+            }
+        }
+    printf("\n");
+    }
+}
